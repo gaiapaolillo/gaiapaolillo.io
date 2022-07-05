53cc2ed0de3cffc7c03394487f805e1da65e0da8
gaiapaolillo <95496731+gaiapaolillo@users.noreply.github.com>
2022-07-05 10:40:49 +0200

    loghi2

--- script_loghi2.js
+++ script_loghi2.js
@@ -29,4 +29,19 @@ checkbox.addEventListener('change', () => {
 /* pulsante darkmode ↑
 --------------------------------
 */
+
+/* solo follia */
+
+$('#click_me').click(function(){
+  $('.item').css({
+    'visibility':'visible'
+  });
+});
+  
+  $('#click_meone').click(function(){
+  $('.item').css({
+    'visibility':'hidden'
+  });
+});
+  
 }
