d8b600f72f9b6ef1d8dda2a7a0e0463fb7d17b27
gaiapaolillo <95496731+gaiapaolillo@users.noreply.github.com>
2022-07-05 10:28:29 +0200

    matite2

--- script_matite2.js
+++ script_matite2.js
@@ -117,4 +117,21 @@ $(document).ready(function(){
   });
 });
    
+
+ /* solo follia */
+
+ $('#click_me').click(function(){
+  $('.item').css({
+    'visibility':'visible'
+  });
+});
+  
+  $('#click_meone').click(function(){
+  $('.item').css({
+    'visibility':'hidden'
+  });
+});
+  
+
+
  }
